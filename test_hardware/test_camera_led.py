4a0bf6c5cea83b6d8733e615f5ef4b9371970feb
RadhikaG <radhikaghosal@gmail.com>
2016-07-06 16:58:13 +0530

    radhika: prototype working woo

--- test_hardware/test_camera_led.py
+++ test_hardware/test_camera_led.py
@@ -9,13 +9,14 @@ camera = picamera.PiCamera()
 # NOTE: Both the visible and ir LEDs are active LOW, hence 0 is ON and vice versa
 # Active LOW is due to NPN transistors
 visible = 2
-ir  = 3
+ir = 3
 
 # pi is initialized as the pigpio object
 pi = pigpio.pi()
 
 pi.set_mode(visible,pigpio.OUTPUT)
 pi.set_mode(ir,pigpio.OUTPUT)
+
 # Defining functions for putting off each LED
 def visibleON():
     # visible is ON and the other is OFF
@@ -27,7 +28,16 @@ def irON():
     pi.write(visible,1)
     pi.write(ir,0)
 
-visibleON()
-time.sleep(5)
-irON()
-time.sleep(5)
+def bothOFF():
+    pi.write(visible, 1)
+    pi.write(ir, 1)
+
+def take_a_pic(pr_tx):
+    irON()
+    camera.capture(pr_tx + '_1.jpg', use_video_port=False)
+    visibleON()
+    camera.capture(pr_tx + '_2.jpg', use_video_port=False)
+    camera.stop_preview()
+
+take_a_pic('blah')
+bothOFF()
